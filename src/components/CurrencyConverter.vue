1632ed919764e83ecfbbca259ac47bc9eb88209c
Gabriel <gabriel.abreu1@hotmail.comm>
2025-01-11 18:35:51 -0300

    add bootstrap and validatios

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,44 +1,108 @@
 <template>
-    <div class="currency-converter">
-        <p>API utilizada : https://app.exchangerate-api.com/ </p>
-        <p>Ultima Atualizacao de Conversao: {{ lastUpdate }}</p>
-      <form @submit.prevent="convertCurrency">
-        <input type="number" v-model="amount" placeholder="Valor" required />
-        <select v-model="fromCurrency">
-            <option v-for="(rate, currency) in currencyOptions" 
-            :key="currency" 
-            :value="currency"
-            >
-            {{ currency }}
-            </option>
-        </select>
-        <select v-model="toCurrency">
-            <option v-for="(rate, currency) in currencyOptions" 
-            :key="currency" 
-            :value="currency"
-            >
-            {{ currency }}
-            </option>
-        </select>
-        <button type="submit">Converter</button>
-      </form>
-      <p v-if="result">{{ amount }} {{ fromCurrency }} = {{ result }} {{ toCurrency }}</p>
-      <p v-if="error" class="error">{{ error }}</p>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-primary text-white text-center">
+                        <h3>Conversor de Moedas</h3>
+                    </div>
+                    <p class="p-1 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-2 text-center">API utilizada : https://app.exchangerate-api.com/ </p>
+                    <p class="text-center">Ultima Atualizacao de Conversao: {{ lastUpdated }}</p>
+
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="amount" class="form-label">Valor</label>
+                                <input 
+                                type="number" 
+                                v-model.number="amount" 
+                                placeholder="Valor" 
+                                required
+                                class="form-control"
+                                />
+                            </div>
+
+                            <div class="mb-3 d-flex align-items-center">
+                                <label for="fromCurrency" class="form-label me-3" style="width: 50px;">De</label>
+                                <select 
+                                    v-model="fromCurrency"
+                                    class="form-select me-2"
+                                    style="max-width: 150px;">
+                                    <option v-for="(rate, currency) in currencyOptions" 
+                                    :key="currency" 
+                                    :value="currency"
+                                    >
+                                    {{ currency }}
+                                    </option>
+                                </select>
+                                <span class="ms-2 text-muted">
+                                    {{currencyNames[fromCurrency]}}
+                                </span>
+                            </div>
+
+                            <div class="mb-3 d-flex align-items-center">
+                                <label for="toCurrency" class="form-label me-3" style="width: 50px;">Para</label>
+                                <select 
+                                    v-model="toCurrency"
+                                    class="form-select me-2"
+                                    style="max-width: 150px;"
+                                    >
+                                    <option v-for="(rate, currency) in currencyOptions" 
+                                    :key="currency" 
+                                    :value="currency"
+                                    >
+                                    {{ currency }}
+                                    </option>
+                                </select>
+                                <span class="ms-2 text-muted">
+                                    {{currencyNames[toCurrency]}}
+                                </span>
+                            </div>
+
+                            <button
+                            class="btn btn-primary w-100"
+                            type="button"
+                            @click="convertCurrency"
+                            >
+                            Converter
+                            </button>
+                        </form>
+                    </div>
+
+                    <div class="card-footer text-center">
+                        <div v-if="result" class="text-success">
+                            <div v-if="conversionRate" class="mt-3 text-center">
+                                <p class="text-info">Taxa de conversão: 1 {{ fromCurrency }} = {{ conversionRate }} {{ toCurrency }}</p>
+                            </div>
+                            <div>
+                                Resultado : {{ amount }} {{ fromCurrency }} = {{ result }} {{ toCurrency }}
+                            </div>
+                        </div>
+                        <p v-if="error" class="text-danger">
+                            {{ error }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </template>
   
   <script>
+  import currencyNames from '@/assets/currencies.json';
 
   export default {
     data() {
       return {
-        amount: '',
+        amount: 0,
         fromCurrency: 'USD',
         toCurrency: 'BRL',
         result: null,
         error: null,
         currencyOptions: {},
-        lastUpdate: '',
+        conversionRate: null,
+        lastUpdated: null,
+        currencyNames: currencyNames,
       };
     },
     created() {
@@ -55,10 +119,12 @@
                 const response = await this.getRequest()
                 const data = await response.json()
                 if (data.result === 'success') {
-                this.lastUpdate = data.time_last_update_utc
-                this.currencyOptions = data.conversion_rates
+                    const rawDate = data.time_last_update_utc;
+                    this.lastUpdated = rawDate.split(' ').slice(0, 4).join(' ');
+
+                    this.currencyOptions = data.conversion_rates
                 } else {
-                this.error = 'Erro ao buscar opções de moedas.'
+                    this.error = 'Erro ao buscar opções de moedas.'
                 }
             } catch (err) {
                 console.error(err);
@@ -66,6 +132,11 @@
             }
         },
         async convertCurrency() {
+
+            if (this.amount <= 0) {
+                this.error = "Por favor, insira um valor maior que 0 para converter.";
+                return;
+            }
             try {
                 // const response = await fetch(url);
                 const data = await this.getRequest().then((response) => response.json())
@@ -74,6 +145,7 @@
                 if (data.result === 'success') {
                 // Obter a taxa de conversão para a moeda de destino
                 const rate = data.conversion_rates[this.toCurrency]
+                this.conversionRate = rate;
                 if (rate) {
                     this.result = (this.amount * rate).toFixed(2)
                     this.error = null;
